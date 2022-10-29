379102a296df4a6369a3d06f307572f43264da51
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-29 13:36:13 +0200

    updated server file and created env file

--- server/server.js
+++ server/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 
 const app = express();
 import dotenv from 'dotenv';
+import connectDB from './db/connect';
 dotenv.config();
 
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
       console.log(`server is running on port ${port}`);
     });
