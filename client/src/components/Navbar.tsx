20704e80808538a396100af3539233376f21e3fd
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-31 11:55:39 +0100

    added shopping cart in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,15 @@
+import { FaShoppingCart } from 'react-icons/fa';
+
 const Navbar = () => {
   return (
     <div className=' p-5 flex justify-between bg-black'>
       <div> Logo </div>
-      <div> Login / Register </div>
+      <div>
+        <div className='flex items-center gap-5'>
+          <p>Login / Register</p>
+          <FaShoppingCart />
+        </div>
+      </div>
     </div>
   );
 };
