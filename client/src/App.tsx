98ea80276c2b207deac265e24966040f429c00fe
Ayrow <aymeric.pilaert@hotmail.fr>
2022-10-31 12:01:55 +0100

    updated app file to add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   );
 };
 export default App;
