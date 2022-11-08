dd514909681f10471ae4bf2fb6abf0174128ed7d
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-08 10:24:06 +0100

    added fake logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import { FaShoppingCart } from 'react-icons/fa';
+import Logo from '../assets/logo.svg';
 
 const Navbar = () => {
   return (
-    <div className=' p-5 flex justify-between bg-black'>
-      <div> Logo </div>
+    <div className=' py-2 px-5 flex justify-between items-center bg-black'>
+      <div>
+        <img src={Logo} className='w-20' alt='logo' />
+      </div>
       <div>
         <div className='flex items-center gap-5'>
           <p>Login / Register</p>
