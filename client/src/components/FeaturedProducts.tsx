980e2592d29a12faaa4c710404fbdec82de07800
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-09 11:59:30 +0100

    setup product card

--- client/src/components/FeaturedProducts.tsx
+++ client/src/components/FeaturedProducts.tsx
@@ -1,7 +1,14 @@
+import ProductCard from './ProductCard';
+
 const FeaturedProducts = () => {
   return (
-    <div className=' bg-neutral-600 pt-5'>
+    <div className=' bg-neutral-600 py-5'>
       <h2 className='text-center text-xl'>Featured Products</h2>
+      <div className='mt-10 flex justify-around flex-wrap'>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </div>
   );
 };
--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,4 +1,10 @@
 const ProductCard = () => {
-  return <div>ProductCard</div>;
+  return (
+    <div className='flex flex-col items-center gap-5 border rounded-lg shadow-lg p-5'>
+      <p>product image</p>
+      <h4>Nice shirt</h4>
+      <button className='btn'>Add to cart</button>
+    </div>
+  );
 };
 export default ProductCard;
