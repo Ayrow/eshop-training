bb4629d6e461c0ab326b650cf0b2118e5ea75875
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-09 11:46:34 +0100

    added some changes to header for homepage

--- client/src/components/HomeHeader.tsx
+++ client/src/components/HomeHeader.tsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom';
 
-const HomeHeader = () => {
+const HomeHeader: React.FC = () => {
   return (
-    <div className='grid grid-cols-2'>
-      <div>
+    <div className=' h-full flex justify-center'>
+      <div className='flex flex-col items-center'>
         <h1 className='text-xl'>The best products for you</h1>
-        <p>Discover new collections</p>
-        <Link to='/products'>Check products</Link>
+        <p className='mt-2'>Discover new collections</p>
+        <Link to='/products' className='btn mt-5'>
+          Check products
+        </Link>
       </div>
-      <div className='flex justify-center'>Image section</div>
     </div>
   );
 };
