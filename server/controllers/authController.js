077dbef69dd7876890e83b832673b8682aaffb98
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-10 15:55:48 +0100

    setup basic auth controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,9 @@
+import User from '../models/User.js';
+
+const register = async (req, res) => {
+  res.status(200).json({ msg: 'ok' });
+};
+
+const login = async (req, res) => {
+  res.status(200).json({ msg: 'ok' });
+};
