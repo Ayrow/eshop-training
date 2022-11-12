745ae16b5f8cc93b4e190e86007d97f68156cab3
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-12 16:48:05 +0100

    changed some elements to display a registering form

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const RegisterForm: React.FC<Props> = ({ setIsMember }) => {
-  const handleSignin = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
   return (
@@ -15,7 +15,7 @@ const RegisterForm: React.FC<Props> = ({ setIsMember }) => {
         Register a new account
       </div>
       <div className='mt-8'>
-        <form onSubmit={(e) => handleSignin(e)} autoComplete='off'>
+        <form onSubmit={(e) => handleRegister(e)} autoComplete='off'>
           <div className='flex flex-col mb-2'>
             <div className='flex relative '>
               <span className='rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm'>
@@ -42,18 +42,11 @@ const RegisterForm: React.FC<Props> = ({ setIsMember }) => {
               />
             </div>
           </div>
-          <div className='flex items-center mb-6 -mt-4'>
-            <div className='flex ml-auto'>
-              <p className='inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white'>
-                Forgot Your Password?
-              </p>
-            </div>
-          </div>
           <div className='flex w-full'>
             <button
               type='submit'
               className='py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg '>
-              Login
+              Register
             </button>
           </div>
         </form>
