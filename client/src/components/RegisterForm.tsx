21b45c0e7f1e6187f8f08abaf0c9a321939d4c47
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-15 17:32:40 +0100

    register form

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -23,7 +23,6 @@ const RegisterForm: React.FC<Props> = ({ setIsMember }) => {
               </span>
               <input
                 type='text'
-                id='sign-in-email'
                 className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
                 placeholder='Your email'
               />
@@ -36,7 +35,6 @@ const RegisterForm: React.FC<Props> = ({ setIsMember }) => {
               </span>
               <input
                 type='password'
-                id='sign-in-email'
                 className=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
                 placeholder='Your password'
               />
