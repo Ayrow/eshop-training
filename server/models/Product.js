0fc10e5dc703e984f95c6a91334fa0b487ddaa50
Ayrow <aymeric.pilaert@hotmail.fr>
2022-12-02 12:33:17 +0100

    updated product model and started populatejs to add products to db

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
 const ProductSchema = new mongoose.Schema({
-  name: String,
+  title: String,
   price: Number,
+  description: String,
+  category: String,
+  image: String,
 });
 
 export default mongoose.model('Product', ProductSchema);
