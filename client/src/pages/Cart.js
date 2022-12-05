982a3a6f1da9023900456cb36284a864ab440e9f
Ayrow <aymeric.pilaert@hotmail.fr>
2022-12-05 10:06:31 +0100

    conditional rendering cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,4 +1,26 @@
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const navigate = useNavigate();
+  const { user } = useSelector((store) => store.user);
+  return (
+    <div className='mt-5'>
+      {user ? (
+        <div>
+          <h2 className='text-xl text-center'>Cart</h2>
+        </div>
+      ) : (
+        <div className='text-center'>
+          <p>You need an account to check your cart</p>
+          <button
+            onClick={() => navigate('/login')}
+            className='mt-5 p-2 bg-orange-500 hover:bg-orange-400 border rounded-md'>
+            Register
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 export default Cart;
