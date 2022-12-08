a937a414fb68bb3880ec2e0d384295d67510cddb
Ayrow <aymeric.pilaert@hotmail.fr>
2022-12-08 14:52:19 +0100

    added basic getProductsFromCart function in express

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,6 +1,15 @@
 import User from '../models/User.js';
+import Product from '../models/Product.js';
 
-const getProductsFromCart = (req, res) => {
+const getProductsFromCart = async (req, res) => {
+  const id = req.user.userId;
+
+  const user = await User.findOne({ _id: id });
+  const userCart = user.cart;
+  const productsInCart = await Product.find({ _id: userCart });
+
+  console.log('userCart', { productsInCart, user });
+
   res.status(200).json({ msg: 'get all products from cart' });
 };
 
